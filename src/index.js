83fd97b39a110e4c14cf17782ee60b2c5ae97bce
PsychoRoy <victor.martins.vc@gmail.com>
2020-07-29 03:32:55 -0300

    Segunda Aula

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import {BrowserRouter, Switch, router, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+
+// Desafio master blaster do jooj top
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+    <Route path= "/" component={Home} exact />
+      <Route path= "/cadastro/video" component={CadastroVideo} />
+      <Route path= "/cadastro/Categoria" component={CadastroCategoria} />
+      <Route  component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
