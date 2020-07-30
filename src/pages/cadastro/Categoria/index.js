f8b520a730b101c8679217304d95a989c0fd3ec6
PsychoRoy <victor.martins.vc@gmail.com>
2020-07-30 10:40:54 -0300

    Aula Três - Cadastro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Test']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+
+      }}>
 
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value={nomeDaCategoria}
+            onChange={function funcHandlerQueOErroPediu(infosDoEvento) {
+              setNomeDaCategoria(infosDoEvento.target.value);
+              
+            }}
           />
         </label>
 
@@ -20,7 +34,15 @@ function CadastroCategoria() {
           Cadastrar
         </button>
       </form>
-
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return(
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })} 
+      </ul>
 
       <Link to="/">
         Ir para home
